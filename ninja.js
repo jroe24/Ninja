d5e21b6dba704d4495a963f0930d84ff4d8d17ea
Jacob Roe <jroenavy24@gmail.com>
2021-07-08 10:47:14 -0700

    initial commit

---
+++ ninja.js
@@ -0,0 +1,27 @@
+class Ninja {
+    constructor(name, health = 50, speed = 3, strength = 3) {
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
+    }
+
+    sayName(){
+        console.log(this.name);
+        return this;
+    }
+
+    showStats(){
+        console.log("Name: " + this.name + ", Strength: " + this.strength + ", Speed: " + this.speed + ", Health: " + this.health);
+        return this;
+    }
+    
+    drinkSake(){
+        this.health += 10;
+        return this;
+    }
+}
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
